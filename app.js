ec36668bbdd7d3bc32234ae2e0e499e6704f93fb
nityak1996 <nityak1996@gmail.com>
2024-10-26 19:07:34 +0530

    get,post,patch,delete

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
-const express = require("express")
-const app = express()
-const PORT = 5000
+const express = require("express");
+const app = express();
 
-app.use("/",(req,res)=>{
-    res.send("Hello nitya singh")
+//order of code is matter alot !!!
+app.get("/test",(req,res)=>{
+    res.send("Hello from the server")
 })
 
+app.get("/user",(req,res)=>{
+    res.send({firstName:"nitya ", lastName:"singh" })
+})
+app.post("/user",(req,res)=>{
+    res.send("Data is successfully save to databse")
+})
+app.patch("/user",(req,res)=>{
+    res.send("Data is successfully updated")
+})
+app.delete("/user",(req,res)=>{
+    res.send("Data is successfully deletd")
+})
 
-
-app.listen(PORT,()=>{console.log(`server will be started at ${PORT}`)})
+const PORT = 5000;
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
