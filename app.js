c5c28098e03b77c6b2d31ede4cae6f7b149f9262
nityak1996 <nityak1996@gmail.com>
2024-10-26 22:01:09 +0530

    handeling router with next function

--- app.js
+++ app.js
@@ -1,25 +1,31 @@
 const express = require("express");
 const app = express();
 
-//order of code is matter alot !!!
-app.get("/test",(req,res)=>{
-    res.send("Hello from the server")
-})
-
-app.get("/user",(req,res)=>{
-    res.send({firstName:"nitya ", lastName:"singh" })
-})
-app.post("/user",(req,res)=>{
-    res.send("Data is successfully save to databse")
-})
-app.patch("/user",(req,res)=>{
-    res.send("Data is successfully updated")
-})
-app.delete("/user",(req,res)=>{
-    res.send("Data is successfully deletd")
-})
+app.get(
+  "/user",
+  (req, res,next) => {
+    console.log("One")
+    res.send("Handeling the route user1");
+    next()
+  },
+  (req, res,next) => {
+    console.log("two")
+    // res.send("Handeling the route user2");
+    next()
+  },
+  (req, res,next) => {
+    console.log("three")
+    res.send("Handeling the route user 3");
+    next()
+    
+  },
+  (req,res)=>{
+    console.log("four")
+    res.send("Handelin the router user 4")
+  }
+);
 
 const PORT = 5000;
 app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running on http://localhost:${PORT}`);
 });
